721e359abaea992e6a286bb1e2646a0cbecfae0f
Jorjonidas26 <jor.olmedoc@gmail.com>
2025-10-06 18:16:28 -0700

    Add files via upload

---
+++ follow_the_numbers/numbers.py
@@ -0,0 +1,42 @@
+import pgzrun
+from random import randint
+
+WIDTH = 800
+HEIGHT = 800
+
+dots = []
+lines = []
+
+next_dot = 0
+
+for dot in range(0, 10):
+    actor = Actor("dot")
+    actor.pos = randint(20, WIDTH - 20), \
+    randint(20, HEIGHT - 20)
+    dots.append(actor)
+
+def draw():
+    screen.fill("Black")
+    number = 1
+    for dot in dots:
+        screen.draw.text(str(number), (dot.pos[0], dot.pos[1] + 12))
+        
+        dot.draw()
+        number = number + 1
+    for line in lines:
+        screen.draw.line(line[0], line[1], (100, 0, 0))
+    
+    dot.draw()
+    number = number + 1
+    for line in lines:
+        screen.draw.line(line[0], line[1], (100,0,0))
+ 
+ 
+def on_mouse_down(pos):
+    global next_dot, lines
+    
+    
+    
+    
+       
+pgzrun.go()
